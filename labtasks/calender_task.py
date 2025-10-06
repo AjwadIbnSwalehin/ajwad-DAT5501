4c41bbab851d15b001938bf0ffa1bfb974af0b79
AjwadIbnSwalehin <aibnswalehin@gmail.com>
2025-10-06 12:19:22 +0100

    Calender task labtask

---
+++ labtasks/calender_task.py
@@ -0,0 +1,40 @@
+def calender(days, start_day):
+    day_dictionary = {
+        "Sunday": 0,
+        "Monday": 1,
+        "Tuesday": 2,
+        "Wednesday": 3,
+        "Thursday": 4,
+        "Friday": 5,
+        "Saturday": 6
+    }
+    
+    # We will need one more week if start day is late in the week
+    calender_print = [
+        ['S', 'M', 'T', 'W', 'T', 'F', 'S'],
+        ['--', '--', '--', '--', '--', '--', '--'],
+        ['--', '--', '--', '--', '--', '--', '--'],
+        ['--', '--', '--', '--', '--', '--', '--'],
+        ['--', '--', '--', '--', '--', '--', '--'],
+        ['--', '--', '--', '--', '--', '--', '--'] 
+    ]
+    
+    starting_index = day_dictionary[start_day]
+    week = 1
+    day_of_month = 1
+    
+    for i in range(starting_index, starting_index + days):
+        # Does not attempt to access weeks beyond our array size
+        if week < len(calender_print):
+            calender_print[week][i % 7] = day_of_month
+        day_of_month += 1
+        
+        # If week is full, change to a new week
+        if (i + 1) % 7 == 0 and day_of_month <= days:
+            week += 1
+            
+    # Print Calender
+    for row in calender_print:
+        print(row)
+
+calender(30, "Monday")
